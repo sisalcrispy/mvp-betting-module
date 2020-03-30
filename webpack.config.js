ad8a48462139f2de088dddae3efe0d881751ccfd
Ludovico Tamigio <ludovico.tamigio@crispybacon.it>
2020-03-30 15:17:23 +0200

    feat: refactor webpack config + add port configuration of dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,38 @@
 const path = require("path");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const PortfinderSync = require('portfinder-sync');
 
+
+/*
+| -------------------------------------------------------
+| Webpack Loader configuration:
+| _______________________________________________________
+ */
+const styleLoader = {
+    loader: 'style-loader'
+};
+const cssLoader = {
+    loader: "css-loader"
+};
+const sassLoader = {
+    loader: 'sass-loader',
+    options: {
+        sourceMap: true
+    }
+};
+
+
+/*
+| -------------------------------------------------------
+| Env parameters
+| -------------------------------------------------------
+ */
+
+const entry = {
+    prod: path.resolve(__dirname, 'src/export.ts'),
+    dev: path.resolve(__dirname, 'src/index.ts'),
+};
 
 const output = {
     prod: {
@@ -11,18 +42,35 @@ const output = {
         libraryTarget: 'commonjs2'
     },
     dev: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist/'),
         filename: 'main.js',
         publicPath: '/',
     }
 };
 
+const externals = {
+    dev: {},
+    prod: {
+        "react": "react"
+    }
+};
+
+/*
+| -------------------------------------------------------
+| Dev server configuration:
+| -------------------------------------------------------
+ */
+
+const devServer = {
+    port: PortfinderSync.getPort(8000),
+    historyApiFallback: true,
+};
 
 module.exports = (env, argv) => {
     const isDev = argv.mode === "development";
 
     return ({
-        entry: "./src/index.tsx",
+        entry: isDev ? entry.dev : entry.prod,
         mode: argv.mode,
         module: {
             rules: [
@@ -32,26 +80,19 @@ module.exports = (env, argv) => {
                     use: "ts-loader",
                 },
                 {
-                    test: /\.s(a|c)ss$/,
-                    exclude: /\.module.(s(a|c)ss)$/,
-                    loader: [
-                        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                        'css-loader',
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: isDev
-                            }
-                        }
+                    test: /\.s?css$/,
+                    use: [
+                        isDev ? styleLoader : MiniCssExtractPlugin.loader,
+                        cssLoader,
+                        sassLoader
                     ]
                 }
             ]
         },
         resolve: {extensions: ["*", ".ts", ".tsx", ".js", ".jsx", ".scss"]},
         output: isDev ? output.dev : output.prod,
-        devServer: {
-            historyApiFallback: true
-        },
+        externals: isDev ? externals.dev : externals.prod,
+        devServer: devServer,
         plugins: [
             new MiniCssExtractPlugin({
                 filename: isDev ? '[name].css' : '[name].[hash].css',
